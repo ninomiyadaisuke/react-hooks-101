dcc47b5650ef37dfbb0f92231cee429c1d7548d3
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-01 11:09:02 +0900

    create operationLogs reducer

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,8 @@ import reducer from '../reducers'
 
 const App = () => {
   const inistialState = {
-    events: []
+    events: [],
+    operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, inistialState)
   
